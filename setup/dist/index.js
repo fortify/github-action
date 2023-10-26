023274d7ded34753c323d3331c423437e1ca3a80
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2023-10-26 17:47:29 +0200

    chore: Add logging

--- setup/dist/index.js
+++ setup/dist/index.js
@@ -6668,8 +6668,11 @@ const FCLI_SHA256 = {
 function updateFcliVersionAliases() {
     for (const slug in Object.keys(FCLI_SHA256)) {
         const version = slug.substring(0, slug.indexOf('/'));
-        TOOLS["fcli"]["versionAliases"][version.substring(0, version.indexOf('.'))] = version;
-        TOOLS["fcli"]["versionAliases"][version.substring(0, version.lastIndexOf('.'))] = version;
+        const alias1 = version.substring(0, version.indexOf('.'));
+        const alias2 = version.substring(0, version.lastIndexOf('.'));
+        core.info(`${version} (${slug}) aliases: ${alias1} ${alias2}`);
+        TOOLS["fcli"]["versionAliases"][alias1] = version;
+        TOOLS["fcli"]["versionAliases"][alias2] = version;
     }
 }
 /**
--- setup/src/setup.ts
+++ setup/src/setup.ts
@@ -84,8 +84,11 @@ const FCLI_SHA256: Record<string, string> = {
 function updateFcliVersionAliases() {
 	for ( const slug in Object.keys(FCLI_SHA256) ) {
 		const version = slug.substring(0, slug.indexOf('/'));
-		TOOLS["fcli"]["versionAliases"][version.substring(0, version.indexOf('.'))] = version;
-		TOOLS["fcli"]["versionAliases"][version.substring(0, version.lastIndexOf('.'))] = version;
+		const alias1 = version.substring(0, version.indexOf('.'));
+		const alias2 = version.substring(0, version.lastIndexOf('.'));
+		core.info(`${version} aliases: ${alias1} ${alias2}`);
+		TOOLS["fcli"]["versionAliases"][alias1] = version;
+		TOOLS["fcli"]["versionAliases"][alias2] = version;
 	}
 }
 
