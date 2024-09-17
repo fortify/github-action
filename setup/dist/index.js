cff9775c36496f3a408165357b937b5771bef8b5
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2024-09-17 11:07:01 +0200

    chore: Disable signature check for fcli dev-versions

--- setup/dist/index.js
+++ setup/dist/index.js
@@ -19080,6 +19080,7 @@ exports.FcliRunOutput = exports.InternalFcliHelper = void 0;
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const exec = __importStar(__nccwpck_require__(1514));
 const tc = __importStar(__nccwpck_require__(7784));
+const core = __importStar(__nccwpck_require__(2186));
 const node_stream_zip_1 = __importDefault(__nccwpck_require__(8119));
 const yaml = __importStar(__nccwpck_require__(5065));
 const constants = __importStar(__nccwpck_require__(9042));
@@ -19363,26 +19364,31 @@ _ArtifactDescriptor_instances = new WeakSet(), _ArtifactDescriptor_getDestDir = 
 }, _ArtifactDescriptor_verify = function _ArtifactDescriptor_verify(file) {
     var e_1, _c;
     return __awaiter(this, void 0, void 0, function* () {
-        const verifier = crypto.createVerify('RSA-SHA256');
-        const readable = fs.createReadStream(file);
-        try {
-            // For some reason, readable.pipe(verifier) doesn't work
-            for (var readable_1 = __asyncValues(readable), readable_1_1; readable_1_1 = yield readable_1.next(), !readable_1_1.done;) {
-                const chunk = readable_1_1.value;
-                verifier.update(chunk);
-            }
+        if (this.version.startsWith("dev_")) {
+            core.warning("Fcli action-default set to development version, not verifying signature. This should be changed to fcli release version before releasingthe GitHub Action.");
         }
-        catch (e_1_1) { e_1 = { error: e_1_1 }; }
-        finally {
+        else {
+            const verifier = crypto.createVerify('RSA-SHA256');
+            const readable = fs.createReadStream(file);
             try {
-                if (readable_1_1 && !readable_1_1.done && (_c = readable_1.return)) yield _c.call(readable_1);
+                // For some reason, readable.pipe(verifier) doesn't work
+                for (var readable_1 = __asyncValues(readable), readable_1_1; readable_1_1 = yield readable_1.next(), !readable_1_1.done;) {
+                    const chunk = readable_1_1.value;
+                    verifier.update(chunk);
+                }
             }
-            finally { if (e_1) throw e_1.error; }
-        }
-        if (!verifier.verify(constants.TOOL_DEFINITIONS_PUBLIC_KEY, this.rsa_sha256, 'base64')) {
-            console.log(constants.TOOL_DEFINITIONS_PUBLIC_KEY);
-            console.log(this.rsa_sha256);
-            throw `File signature verification failed for ${this.downloadUrl}`;
+            catch (e_1_1) { e_1 = { error: e_1_1 }; }
+            finally {
+                try {
+                    if (readable_1_1 && !readable_1_1.done && (_c = readable_1.return)) yield _c.call(readable_1);
+                }
+                finally { if (e_1) throw e_1.error; }
+            }
+            if (!verifier.verify(constants.TOOL_DEFINITIONS_PUBLIC_KEY, this.rsa_sha256, 'base64')) {
+                console.log(constants.TOOL_DEFINITIONS_PUBLIC_KEY);
+                console.log(this.rsa_sha256);
+                throw `File signature verification failed for ${this.downloadUrl}`;
+            }
         }
     });
 };
