2d91e3c5c405391e5ee2cfe725a77b0ded38dcd0
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2024-10-30 18:38:59 +0100

    chore: Update tool versions
    fix: Update Debricked CLI to 2.1.7
    
    feat: Update fcli to 2.9.0
    
    fix: Improve parsing of boolean flags in `*_EXTRA_OPTS`
    
    feat: FoD: Add support for creating application through `DO_SETUP`
    
    feat: FoD: Improve handling of `--copy-from` option in `SETUP_EXTRA_OPTS`

--- setup/dist/index.js
+++ setup/dist/index.js
@@ -18952,7 +18952,7 @@ exports.TOOLS = {
      *            ScanCentral Client, but please double-check.
      */
     "fcli": {
-        "versionAliases": { "action-default": "2.8.0" },
+        "versionAliases": { "action-default": "2.9.0" },
         "cmds": { "windows": "fcli.exe", "linux": "fcli", "darwin": "fcli" }
     },
     "sc-client": {
@@ -18972,7 +18972,7 @@ exports.TOOLS = {
         "cmds": { "windows": "FortifyBugTrackerUtility.bat", "linux": "FortifyBugTrackerUtility", "darwin": "FortifyBugTrackerUtility" }
     },
     "debricked-cli": {
-        "versionAliases": { "action-default": "2.1.6" },
+        "versionAliases": { "action-default": "2.1.7" },
         "cmds": { "windows": "debricked.exe", "linux": "debricked", "darwin": "debricked" }
     }
 };
--- setup/src/constants.ts
+++ setup/src/constants.ts
@@ -15,7 +15,7 @@ export const TOOLS: Record<string, Record<string, Record<string, string>>> = {
      *            ScanCentral Client, but please double-check. 
      */
     "fcli": { 
-        "versionAliases": {"action-default": "2.8.0"},
+        "versionAliases": {"action-default": "2.9.0"},
         "cmds": {"windows": "fcli.exe", "linux": "fcli", "darwin": "fcli"}
     },
     "sc-client": { 
@@ -35,7 +35,7 @@ export const TOOLS: Record<string, Record<string, Record<string, string>>> = {
         "cmds": {"windows": "FortifyBugTrackerUtility.bat", "linux": "FortifyBugTrackerUtility", "darwin": "FortifyBugTrackerUtility"}
     },
     "debricked-cli": { 
-        "versionAliases": {"action-default": "2.1.6"},
+        "versionAliases": {"action-default": "2.1.7"},
         "cmds": {"windows": "debricked.exe", "linux": "debricked", "darwin": "debricked"}
     }
 };
